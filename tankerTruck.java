0b8300741b260edf91dbe005c4d29fa66d238b5b
MorganHurley8 <mohurley2@gmail.com>
2017-09-22 08:33:56 -0600

    Add files via upload

---
+++ tankerTruck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ class ScannerTest {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String name= sc.nextString();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your birthdate?");
+        int birthday = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How long would you like to live?");
+        int life = sc.nextInt;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many ounces of fluid do you consume in a day?");
+        int ounces = sc.nextInt;
+        tankerTruck = int(1024000);
+        totalOunces = (life)*365 * (ounces);
+        totalTanker = (totalOunces)/(totalTanker);
+        System.out.println totalTanker;
+
+
+    }
+}
